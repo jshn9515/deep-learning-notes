64047d532371df8ce8f1efd4c26d0d0dec5f4fc2
jshn9515 <jshn9510@gmail.com>
2026-05-07 17:42:22 -0400

    Update __init__.py to include autoencoder and transformer modules

--- dnnl/dnnl/nn/__init__.py
+++ dnnl/dnnl/nn/__init__.py
@@ -1,5 +1,6 @@
 from .attention import *
+from .autoencoder import *
 from .diffusion import *
 from .positional_encoding import *
+from .transformer import *
 from .unet import *
-from .vae import *
