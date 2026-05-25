5afaea53e1537fa30f2ac144b8ade81433311c48
jshn9515 <jshn9510@gmail.com>
2026-05-25 00:04:49 -0400

    Refactor imports and update __all__ in functional module for clarity

--- dnnl/src/dnnl/nn/functional/__init__.py
+++ dnnl/src/dnnl/nn/functional/__init__.py
@@ -1,2 +1,19 @@
-from .attention import *
-from .flash_attention_v1 import *
+from .attention import (
+    attention,
+    generate_causal_mask,
+    multi_head_attention,
+    scaled_dot_product_attention,
+)
+from .flash_attention_v1 import (
+    flash_attention_v1_backward,
+    flash_attention_v1_forward,
+)
+
+__all__ = [
+    'attention',
+    'generate_causal_mask',
+    'multi_head_attention',
+    'scaled_dot_product_attention',
+    'flash_attention_v1_backward',
+    'flash_attention_v1_forward',
+]
