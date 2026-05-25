9e2f537c1e6b9450ba97a1c678f9d8db29321c8e
jshn9515 <jshn9510@gmail.com>
2026-05-25 00:04:53 -0400

    Refactor imports and define __all__ for explicit module exports

--- dnnl/src/dnnl/nn/__init__.py
+++ dnnl/src/dnnl/nn/__init__.py
@@ -1,2 +1,21 @@
-from .attention import *
-from .transformer import *
+from .attention import MultiheadAttention
+from .transformer import (
+    LearnablePositionalEmbedding,
+    SinusoidalPositionalEncoding,
+    Transformer,
+    TransformerDecoder,
+    TransformerDecoderLayer,
+    TransformerEncoder,
+    TransformerEncoderLayer,
+)
+
+__all__ = [
+    'MultiheadAttention',
+    'LearnablePositionalEmbedding',
+    'SinusoidalPositionalEncoding',
+    'Transformer',
+    'TransformerDecoder',
+    'TransformerDecoderLayer',
+    'TransformerEncoder',
+    'TransformerEncoderLayer',
+]
