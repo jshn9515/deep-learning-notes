951e8354eaa37d180a73093b2ce0ce6346fdf29e
jshn9515 <jshn9510@gmail.com>
2026-05-28 22:40:51 -0400

    Refactor dataset download error handling in download_datasets.py

--- utils/download_datasets.py
+++ utils/download_datasets.py
@@ -1,15 +1,20 @@
-import os
-
 import torchvision.datasets as datasets
 
-ROOT = os.getenv('DNNL_DATA_ROOT', os.path.expanduser('~/datasets'))
+import dnnl
+
+ROOT = dnnl.get_data_root()
+
+
+class DatasetDownloadError(RuntimeError):
+    pass
+
 
 try:
     ds = datasets.MNIST(ROOT, download=True)
 except Exception as err:
-    raise ConnectionRefusedError(f'Error downloading MNIST dataset: {err}')
+    raise DatasetDownloadError('Error downloading MNIST dataset.') from err
 
 try:
     ds = datasets.Caltech101(ROOT, download=True)
 except Exception as err:
-    raise ConnectionRefusedError(f'Error downloading Caltech101 dataset: {err}')
+    raise DatasetDownloadError('Error downloading Caltech101 dataset.') from err
