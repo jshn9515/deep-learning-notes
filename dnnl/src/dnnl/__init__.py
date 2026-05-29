ae8052804331f88d0fbb6f4a2b26ca5c22f5c06d
jshn9515 <jshn9510@gmail.com>
2026-05-28 22:42:32 -0400

    Import additional configuration tools in __init__.py for enhanced functionality

--- dnnl/src/dnnl/__init__.py
+++ dnnl/src/dnnl/__init__.py
@@ -1,3 +1,6 @@
 from . import models as models
 from . import nn as nn
+from .configtools import get_data_root as get_data_root
+from .configtools import get_default_device as get_default_device
+from .configtools import set_seed as set_seed
 from .pylabtools import set_matplotlib_format as set_matplotlib_format
