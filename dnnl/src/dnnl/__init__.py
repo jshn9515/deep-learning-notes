a3ef673d0c63fdb5e5c6718e1a90e28213cbc584
jshn9515 <jshn9510@gmail.com>
2026-06-01 00:03:40 -0400

    Add optimizer import in __init__.py

--- dnnl/src/dnnl/__init__.py
+++ dnnl/src/dnnl/__init__.py
@@ -1,5 +1,6 @@
 from . import models as models
 from . import nn as nn
+from . import optim as optim
 from .configtools import get_data_root as get_data_root
 from .configtools import get_default_device as get_default_device
 from .configtools import set_seed as set_seed
