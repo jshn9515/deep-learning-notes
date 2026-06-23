c57244fc5d8fc3b092b911fa0d4716c084b9b331
jshn9515 <jshn9510@gmail.com>
2026-06-23 17:44:32 -0400

    Add tests for optimizer representation and default hyperparameters

--- dnnlpy/tests/optim/test_utils.py
+++ dnnlpy/tests/optim/test_utils.py
@@ -23,3 +23,33 @@ def test_run_optimizer_records_parameter_history():
     assert torch.allclose(history[1], torch.tensor([0.9, -1.8]))
     assert torch.allclose(history[2], torch.tensor([0.81, -1.62]))
     assert torch.allclose(params, torch.tensor([0.81, -1.62]))
+
+
+def test_optimizer_repr_shows_hyperparameters_without_state():
+    params = torch.tensor([1.0, -2.0], requires_grad=True)
+    optimizer = dopt.SGD([params], lr=0.1, momentum=0.9, weight_decay=0.01)
+
+    assert optimizer.defaults == {
+        'lr': 0.1,
+        'momentum': 0.9,
+        'weight_decay': 0.01,
+        'nesterov': False,
+    }
+    assert repr(optimizer) == (
+        'SGD(lr=0.1, momentum=0.9, weight_decay=0.01, nesterov=False)'
+    )
+
+
+def test_optimizer_repr_uses_defaults_for_tuple_hyperparameters():
+    params = torch.tensor([1.0, -2.0], requires_grad=True)
+    optimizer = dopt.Adam([params], lr=0.01, betas=(0.8, 0.95), eps=1e-7)
+
+    assert optimizer.defaults == {
+        'lr': 0.01,
+        'betas': (0.8, 0.95),
+        'eps': 1e-7,
+        'weight_decay': 0.0,
+    }
+    assert repr(optimizer) == (
+        'Adam(lr=0.01, betas=(0.8, 0.95), eps=1e-07, weight_decay=0.0)'
+    )
