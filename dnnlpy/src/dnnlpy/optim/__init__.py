5bb86df01c39c342d58d895b9e173763d59afc9c
jshn9515 <jshn9510@gmail.com>
2026-06-25 18:37:38 -0400

    Remove unused SGD variants from optimizer imports

--- dnnlpy/src/dnnlpy/optim/__init__.py
+++ dnnlpy/src/dnnlpy/optim/__init__.py
@@ -6,10 +6,6 @@ from .base import Optimizer as Optimizer
 from .muon import Muon as Muon
 from .rmsprop import RMSprop as RMSprop
 from .sgd import SGD as SGD
-from .sgd import SimpleSGD as SimpleSGD
-from .sgd import SimpleSGDWithMomentum as SimpleSGDWithMomentum
-from .sgd import SimpleSGDWithNesterovMomentum as SimpleSGDWithNesterovMomentum
-from .sgd import SimpleSGDWithWeightDecay as SimpleSGDWithWeightDecay
 from .utils import collect_lr_schedule as collect_lr_schedule
 from .utils import plot_lr_schedule as plot_lr_schedule
 from .utils import run_optimizer as run_optimizer
