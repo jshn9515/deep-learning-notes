12bfe52652470dbbec28e6f30b8b8ce99bcefb18
jshn9515 <jshn9510@gmail.com>
2026-06-27 17:01:31 -0400

    Enhance docstring for CrossEntropyLoss class and its __init__ method

--- dnnlpy/src/dnnlpy/nn/loss.py
+++ dnnlpy/src/dnnlpy/nn/loss.py
@@ -12,6 +12,8 @@ __all__ = ['CrossEntropyLoss']
 
 
 class CrossEntropyLoss(nn.Module):
+    """Combines `nn.LogSoftmax()` and `nn.NLLLoss()` in one single class."""
+
     def __init__(
         self,
         weight: Tensor | None = None,
@@ -19,6 +21,19 @@ class CrossEntropyLoss(nn.Module):
         *,
         fast: bool = False,
     ):
+        """Initializes the CrossEntropyLoss module.
+
+        Args:
+            weight (Tensor | None): A manual rescaling weight given to each class.If given,
+                it has to be a 1D Tensor assigning weight to each of the classes. Default: None.
+            reduction (str, default: 'mean'): Specifies the reduction to apply to the output.
+                - 'none': no reduction will be applied;
+                - 'mean': the sum of the output will be divided by the number of elements in the output;
+                - 'sum': the output will be summed.
+                Default: 'mean'.
+            fast (bool, default: False): If set to True, will use the fast implementation
+                from torch.nn.functional. Default: False.
+        """
         super().__init__()
         self.weight = weight
         self.reduction = reduction
