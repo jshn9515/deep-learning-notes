5f19669a5816c141212575527eec687f891f28b6
jshn9515 <jshn9510@gmail.com>
2026-07-16 21:49:48 -0400

    Add tests for rotary positional embedding and refactor embedding assertions

--- dnnlpy/tests/test_nn/test_representation.py
+++ dnnlpy/tests/test_nn/test_representation.py
@@ -11,13 +11,65 @@ import dnnlpy.nn as dnn
 import dnnlpy.nn.functional as dF
 
 
-def _assert_functional_forward_and_backward_match(
-    x: Tensor,
-    weight: Tensor,
-    **kwargs: Any,
-) -> None:
-    actual_weight = weight.detach().clone().requires_grad_()
-    expected_weight = weight.detach().clone().requires_grad_()
+def _copy(x: Tensor, mode: bool = True) -> Tensor:
+    """Returns a copy of the input tensor with `requires_grad` set to True."""
+    return x.detach().clone().requires_grad_(mode)
+
+
+def test_rotary_positional_embedding_matches_precalculated_rotation_matrices():
+    x = torch.tensor(
+        [
+            [
+                [1.0, 2.0, 3.0, 4.0],
+                [-1.0, 0.5, 2.0, -3.0],
+                [0.25, -0.75, 1.5, 2.5],
+            ],
+            [
+                [-2.0, 1.0, 0.5, 3.0],
+                [4.0, -1.0, -2.0, 0.25],
+                [1.25, 2.25, -0.5, -1.5],
+            ],
+        ],
+        dtype=torch.float64,
+    )
+
+    # For dim=4 and base=10000, the two pair frequencies are 1 and 0.01.
+    # Each sequence position p uses R(p) = diag(R_2(p), R_2(p / 100)).
+    rotation_matrices = torch.tensor(
+        [
+            [
+                [1.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0],
+                [0.0, 0.0, 1.0, 0.0],
+                [0.0, 0.0, 0.0, 1.0],
+            ],
+            [
+                [0.5403023058681398, -0.8414709848078965, 0.0, 0.0],
+                [0.8414709848078965, 0.5403023058681398, 0.0, 0.0],
+                [0.0, 0.0, 0.9999500004166653, -0.0099998333341667],
+                [0.0, 0.0, 0.0099998333341667, 0.9999500004166653],
+            ],
+            [
+                [-0.4161468365471424, -0.9092974268256817, 0.0, 0.0],
+                [0.9092974268256817, -0.4161468365471424, 0.0, 0.0],
+                [0.0, 0.0, 0.9998000066665778, -0.0199986666933331],
+                [0.0, 0.0, 0.0199986666933331, 0.9998000066665778],
+            ],
+        ],
+        dtype=torch.float64,
+    )
+    expected = torch.einsum('sij,bsj->bsi', rotation_matrices, x)
+
+    actual = dF.rotary_positional_embedding(x)
+    assert_close(actual, expected)
+
+    actual = dnn.RotaryPositionalEmbedding(embed_dim=4)(x)
+    assert_close(actual, expected)
+
+
+def _assert_embedding_matches_torch(x: Tensor, weight: Tensor, **kwargs: Any) -> None:
+    actual_weight = _copy(weight)
+    expected_weight = _copy(weight)
 
     actual = dF.embedding(x, actual_weight, **kwargs)
     expected = F.embedding(x, expected_weight, **kwargs)
@@ -37,7 +89,7 @@ def test_embedding_function_matches_torch_forward_and_backward():
     x = torch.tensor([[0, 2, 4], [3, 1, 2]])
     weight = torch.randn(5, 3, dtype=torch.float64)
 
-    _assert_functional_forward_and_backward_match(x, weight)
+    _assert_embedding_matches_torch(x, weight)
 
 
 @pytest.mark.parametrize('padding_idx', [1, -1])
@@ -45,7 +97,7 @@ def test_embedding_function_matches_torch_with_padding_idx(padding_idx: int):
     x = torch.tensor([[0, 1, 3], [3, 1, 2]])
     weight = torch.randn(4, 2, dtype=torch.float64)
 
-    _assert_functional_forward_and_backward_match(
+    _assert_embedding_matches_torch(
         x,
         weight,
         padding_idx=padding_idx,
@@ -56,7 +108,7 @@ def test_embedding_function_matches_torch_with_scale_grad_by_freq():
     x = torch.tensor([0, 1, 1, 2, 2, 2])
     weight = torch.randn(3, 4, dtype=torch.float64)
 
-    _assert_functional_forward_and_backward_match(
+    _assert_embedding_matches_torch(
         x,
         weight,
         scale_grad_by_freq=True,
@@ -65,11 +117,12 @@ def test_embedding_function_matches_torch_with_scale_grad_by_freq():
 
 def test_embedding_function_matches_torch_with_max_norm():
     x = torch.tensor([[0, 2], [2, 3]])
+
     actual_weight = torch.tensor(
         [[3.0, 4.0], [6.0, 8.0], [0.5, 0.0], [0.0, 12.0]],
         requires_grad=True,
     )
-    expected_weight = actual_weight.detach().clone().requires_grad_()
+    expected_weight = _copy(actual_weight)
 
     actual = dF.embedding(
         x,
@@ -120,6 +173,7 @@ def test_embedding_module_initialization_matches_torch():
 
 def test_embedding_module_matches_torch_forward_and_backward():
     x = torch.tensor([[0, 1, 3, 3], [2, 1, 3, 0]])
+
     custom = dnn.Embedding(
         num_embeddings=4,
         embedding_dim=3,
@@ -155,6 +209,7 @@ def test_embedding_module_matches_torch_forward_and_backward():
 
 def test_fast_embedding_module_matches_torch_forward_and_backward():
     x = torch.tensor([[0, 1, 3, 3], [2, 1, 3, 0]])
+
     custom = dnn.Embedding(
         num_embeddings=4,
         embedding_dim=3,
