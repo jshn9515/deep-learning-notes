061d74e6cdb2d94e229ead13a9b7546e4cec506c
jshn9515 <jshn9510@gmail.com>
2026-07-18 00:44:46 -0400

    Add CS336 Assignment 1 writeup

--- cs336/assignment1/train_bpe_openwebtext.py
+++ cs336/assignment1/train_bpe_openwebtext.py
@@ -1,24 +1,33 @@
+# TODO: Replace cProfile with profiling in Python 3.15. See <PEP 799> for details.
+# TODO: Add lazy import in Python 3.15. See <PEP 810> for details.
+import argparse
+import cProfile
+import pstats
 import time
 from collections.abc import Iterator
 
 import datasets as ds
 import dnnlpy.tokenizers as dltk
+import psutil
 
-__all__ = ['main']
+__all__ = [
+    'train_bpe_openwebtext',
+    'profile_train_bpe_openwebtext',
+]
 
 NUM_STORIES = 8013769
 VOCAB_SIZE = 32000
-INITIAL_ALPHABET = dltk.ByteLevelPreTokenizer.alphabet()
 
 
-def batch_iterator(dataset: ds.Dataset, batch_size: int = 1024) -> Iterator[list[str]]:
+def _batch_iterator(dataset: ds.Dataset, batch_size: int = 1024) -> Iterator[list[str]]:
     """Yield batches of story strings to train_from_iterator."""
     for start in range(0, len(dataset), batch_size):
         end = min(start + batch_size, len(dataset))
         yield dataset[start:end]['text']
 
 
-def main():
+def train_bpe_openwebtext() -> dltk.Tokenizer:
+    """Train a BPE tokenizer on the OpenWebText dataset."""
     dataset = ds.load_dataset('Skylion007/openwebtext', split='train', num_proc=4)
     assert len(dataset) == NUM_STORIES
 
@@ -27,21 +36,56 @@ def main():
         pre_tokenizer=dltk.ByteLevelPreTokenizer(add_prefix_space=False),
         decoder=dltk.ByteLevelDecoder(),
     )
-    text_iterator = batch_iterator(dataset)
+    text_iterator = _batch_iterator(dataset)
 
+    print('Training tokenizer on OpenWebText...')
     start = time.perf_counter()
     tokenizer.train_from_iterator(
         text_iterator,
         vocab_size=VOCAB_SIZE,
-        initial_alphabet=INITIAL_ALPHABET,
+        initial_alphabet=dltk.ByteLevelPreTokenizer.alphabet(),
     )
     end = time.perf_counter()
-    print(f'Training completed in {start - end:.4f} seconds.')
+    print(f'Training completed in {end - start:.4f} seconds.')
+    print('[NOTE] This should be less than 12 hours for the OpenWebText dataset.')
+    print(f'Tokenizer vocabulary size: {tokenizer.get_vocab_size()}.')
+    print('LRU cache info:', dltk.utils.bytes_to_unicode.cache_info())
+
+    process = psutil.Process()
+    peak_mem = process.memory_info().peak_wset
+    print(f'Peak memory usage: {peak_mem / 1024**3:.4f} GB.')
+    print('[NOTE] This should be less than 100 GB for the OpenWebText dataset.')
 
     file_name = 'bpe_openwebtext.json'
     tokenizer.save(file_name)
     print(f'Tokenizer saved to {file_name}.')
 
+    return tokenizer
+
+
+def profile_train_bpe_openwebtext():
+    """Profile the training of the BPE tokenizer on the OpenWebText dataset."""
+    with cProfile.Profile() as profiler:
+        train_bpe_openwebtext()
+
+    print('\nProfiling results:')
+    stats = pstats.Stats(profiler)
+    stats.strip_dirs()
+    stats.sort_stats('cumtime')
+    stats.print_stats(10)  # Print top 10 functions by cumulative time
+
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(
+        description='Train a BPE tokenizer on the OpenWebText dataset.'
+    )
+    parser.add_argument(
+        '--profile', action='store_true', help='Profile the training process.'
+    )
+    args = parser.parse_args()
+
+    if args.profile:
+        print('Profiling enabled. Training may take longer.')
+        profile_train_bpe_openwebtext()
+    else:
+        train_bpe_openwebtext()
